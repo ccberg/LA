3c2c9714631ea52c7ae5567e3eea26207f5258c2
Chris Berg <c.c.berg@student.tudelft.nl>
2020-11-09 11:49:38 +0100

    Some trace generating code

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Material related to the master thesis of Chris Berg
 
 ## Setup
-1. Clone the ASCAD repo in `../data/` and rename it's root dir to `../data/ASCAD`. Extract the fixed and variable key traces as decribed in their repo.
+1. Clone the ASCAD repo in `/data/` and rename it's root dir to `/data/ASCAD`. Extract the fixed and variable key traces as decribed in their repo.
--- settings/ascad.py
+++ settings/ascad.py
@@ -1,8 +1,12 @@
-import pandas as pd
+import h5py
 
-ASCAD_DATA = "../../data/ASCAD/ATMEGA_AES_v1"
+ASCAD_DATA = "/data/ASCAD/ATMEGA_AES_v1"
 ASCAD_DATA_VAR = "/ATM_AES_v1_variable_key/ASCAD_data/ASCAD_databases"
 ASCAD_DATA_FIX = "/ATM_AES_v1_fixed_key/ASCAD_data/ASCAD_databases"
+
+# Range of the interesting part of the trace, selected by ASCAD.
+# Not sure if this range also holds for the raw_traces datasets.
+ASCAD_SELECTED_DATA_RANGE = range(-128, 127)
 
 
 class ASCADDataType:
@@ -12,11 +16,25 @@ class ASCADDataType:
     desync_100 = "ASCAD_desync100"
 
 
-class ASCADData:
-    @staticmethod
-    def random_key(data_type) -> object:
-        return pd.read_hdf(f"{ASCAD_DATA}{ASCAD_DATA_VAR}/{data_type}.h5")
+class TraceGroup:
+    data_range = []
 
     @staticmethod
-    def fixed_key(data_type) -> object:
-        return pd.read_hdf(f"{ASCAD_DATA}{ASCAD_DATA_FIX}/{data_type}.h5")
+    def random_key(data_type: str) -> h5py.File:
+        pass
+
+    @staticmethod
+    def fixed_key(data_type: str) -> h5py.File:
+        pass
+
+
+class ASCADData(TraceGroup):
+    data_range = ASCAD_SELECTED_DATA_RANGE
+
+    @staticmethod
+    def random_key(data_type=ASCADDataType.default) -> object:
+        return h5py.File(f"{ASCAD_DATA}{ASCAD_DATA_VAR}/{data_type}.h5", 'r')
+
+    @staticmethod
+    def fixed_key(data_type=ASCADDataType.default) -> object:
+        return h5py.File(f"{ASCAD_DATA}{ASCAD_DATA_FIX}/{data_type}.h5", 'r')

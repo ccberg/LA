5a04b0dad8f53f0fc4504ec1c0c992d17d8e3f70
Chris Berg <c.c.berg@student.tudelft.nl>
2020-11-14 12:18:28 +0100

    Added caching of statistical moment results

---
+++ settings/cache.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+
+from settings.constants import ROOT_DIR
+
+
+def cache_np(name, f, *args, v=1, replace=False):
+    f_name = f"{ROOT_DIR}/cache/{name}_{v}.npz"
+    if replace or not os.path.exists(f_name):
+        np.savez_compressed(f_name, data=f(*args))
+
+    with np.load(f_name) as file:
+        return file['data']
+
+
+if __name__ == '__main__':
+    print(cache_np("test", lambda a: np.zeros(a), 100))
+
---
+++ settings/constants.py
@@ -0,0 +1,4 @@
+import os
+from pathlib import Path
+
+ROOT_DIR = os.path.dirname(os.path.abspath(Path(__file__).parent))

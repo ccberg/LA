4538cec784c010759931767e31440d66d45e07ea
Chris Berg <c.c.berg@student.tudelft.nl>
2020-11-17 22:41:16 +0100

    ASCAD chi experiments

--- tools/cache.py
+++ tools/cache.py
@@ -4,10 +4,16 @@ import numpy as np
 from tools.constants import ROOT_DIR
 
 
-def cache_np(name, f, *args, v=1, replace=False):
+def cache_np(name, f=None, *args, v=1, replace=False):
+    """
+    Caches the result of a function. In case no function is supplied,
+    the results of the specified cache name are returned.
+    """
     f_name = f"{ROOT_DIR}/cache/{name}_{v}.npz"
-    if replace or not os.path.exists(f_name):
-        np.savez_compressed(f_name, data=f(*args))
+    if f is not None:
+        # Only try to store the results of f(*args) if f is supplied.
+        if replace or not os.path.exists(f_name):
+            np.savez_compressed(f_name, data=f(*args))
 
     with np.load(f_name) as file:
         return file['data']

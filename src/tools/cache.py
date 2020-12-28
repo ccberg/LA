31d082d26fc74c1ac503dedafef100fbf5f8bc85
Chris Berg <c.c.berg@student.tudelft.nl>
2020-12-28 22:28:26 +0100

    Updated cache tools

--- src/tools/cache.py
+++ src/tools/cache.py
@@ -5,7 +5,7 @@ from src.tools.constants import ROOT_DIR
 import pickle as pkl
 
 
-def get_cache():
+def get_cache_loc():
     return f"{ROOT_DIR}/../.cache"
 
 
@@ -14,7 +14,7 @@ def cache_np(name, f=None, *args, v=1, replace=False):
     Caches the result of a function. In case no function is supplied,
     the results of the specified .cache name are returned.
     """
-    f_name = f"{get_cache()}/{name}_{v}.npz"
+    f_name = f"{get_cache_loc()}/{name}_{v}.npz"
     if f is not None:
         # Only try to store the results of f(*args) if f is supplied.
         if replace or not os.path.exists(f_name):
@@ -25,7 +25,7 @@ def cache_np(name, f=None, *args, v=1, replace=False):
 
 
 def cache_pkl(name, cls_name=None, *args, v=1, replace=False):
-    f_name = f"{get_cache()}/{name}_{v}.pkl"
+    f_name = f"{get_cache_loc()}/{name}_{v}.pkl"
 
     if cls_name is not None:
         # Only try to store the results of cls_name(*args) if cls_name is supplied.
@@ -35,6 +35,17 @@ def cache_pkl(name, cls_name=None, *args, v=1, replace=False):
 
     with open(f_name, "rb") as file:
         return pkl.load(file)
+
+
+class NBCache:
+    def __init__(self, path):
+        self.path = path
+
+    def np(self, name: str, *args):
+        res = cache_np(f"{self.path}/{name}", *args)
+        setattr(self, name, res)
+
+        return res
 
 
 if __name__ == '__main__':

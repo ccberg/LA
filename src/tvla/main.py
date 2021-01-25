013593adaefa5827d73e0ba10c4e3705e9a3fc63
Chris Berg <c.c.berg@student.tudelft.nl>
2021-01-25 21:33:11 +0100

    Removed nb code

---
+++ src/tvla/main.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def device_fails(left: np.array, right: np.array, t: float):
+    return np.array((left < t, right < t)).any()
+
+
+def tvla(test, left, right, p=1, debug=False):
+    """
+    Applies some given statistical test against the given samples.
+
+    @param test: statistical test function that takes two samples and returns test values.
+    @param left: Four sample sets containing s samples representing a power trace of length t.
+    @param right: Two sample sets containing s samples representing a power trace of length t.
+    @param p: Decide whether a device fails using (100 * p)%-confidence interval.
+    @param debug: Set to True if this function should throw Exceptions upon improperly structured input.
+    @return: Whether the device fails on (A against A, A against B).
+    """
+    if debug:
+        assert len(left) == 4
+        assert len(right) == 2
+
+    # Four different samples from distribution A.
+    a, b, c, d = left
+    # Two different samples from distribution B. x and y are compared to a and b, respectively.
+    _, _, x, y = right
+
+    # Test A against A.
+    aa1 = test(a, c)
+    aa2 = test(b, d)
+
+    # Test A against B.
+    ab1 = test(a, x)
+    ab2 = test(b, y)
+
+    t = np.percentile([aa1, aa2, ab1, ab2], p)
+
+    # This value for t lets p% of the devices fail.
+    return device_fails(aa1, aa2, t), device_fails(ab1, ab2, t)
--- src/tvla/t.py
+++ src/tvla/t.py
@@ -7,27 +7,18 @@ from src.tools.stat_moment import get_mvs
 
 
 def make_t_test(n):
-    def test(a, b, p):
-        ma, sa = a
-        mb, sb = b
+    sn = np.sqrt(2 / n)
 
-        s = np.sqrt((sa + sb) / 2)
-        t = (ma - mb) / (s * np.sqrt(2 / n))
-        dof = 2 * n - 2
+    def test(a: np.array, b: np.array):
+        mean_a, var_a = np.moveaxis(a, 0, -1)
+        mean_b, var_b = np.moveaxis(b, 0, -1)
 
-        t_abs = np.abs(t).astype(np.float64)
-        return stats.distributions.t.sf(t_abs, dof) * 2 > p
+        m = mean_a - mean_b
+        s = np.sqrt((var_a + var_b) / 2) * sn
+
+        t = m / s
+
+        return np.abs(t).astype(np.float64)
 
     return test
 
-
-if __name__ == '__main__':
-    sps = np.array([gen_trace(5), gen_trace(5)])
-
-    t_mv = [make_t_test(350)(*mv, .95) for mv in np.moveaxis(get_mvs(sps), 0, 1)]
-    t_base = np.array(stats.stats.ttest_ind(*sps, equal_var=False)[1] > .95)
-
-    # If the sum of mv = 0, the equality assertion between t_mv and t_base would always pass.
-    assert sum(t_mv) > 0
-
-    assert_almost_equal(t_mv, t_base)

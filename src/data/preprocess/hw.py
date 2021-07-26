edc9924b6d6787bf31db6f5e25e48b6020009867
Chris Berg <contact@ccberg.nl>
2021-07-26 22:06:00 +0200

    Changed hamming weight storage to state byte storage

--- src/data/preprocess/hw.py
+++ src/data/preprocess/hw.py
@@ -1,10 +1,11 @@
 import numpy as np
 from tqdm import tqdm
 
+from src.aes.hw_sbox import hw
 from src.aes.main import AES
 
 
-def full_states(plaintexts: np.array, keys: np.array):
+def full_states_hw(plaintexts: np.array, keys: np.array):
     """
     Hamming weight of all state bytes, after the SBox of each round of AES.
     """
@@ -14,7 +15,7 @@ def full_states(plaintexts: np.array, keys: np.array):
         aes = AES(keys[ix])
         aes.encrypt(plaintexts[ix])
 
-        hws[ix] = aes.hw_after_round
+        hws[ix] = [hw(i) for i in aes.state_after_round]
 
     return hws
 
@@ -23,5 +24,34 @@ def third_byte(plaintexts: np.array, keys: np.array):
     """
     Hamming weight of the third state bytes, after the SBox of each round of AES.
     """
-    return full_states(plaintexts, keys)[:, :, 2]
+    return full_states_hw(plaintexts, keys)[:, :, 2]
 
+
+BYTE_LEN = 256
+
+
+def full_states(plaintexts: np.array, keys: np.array):
+    """
+    All state bytes, after the SBox of each round of AES-128.
+    """
+    states = np.zeros((len(plaintexts), 10, 16), dtype=np.uint8)
+
+    for ix in tqdm(range(len(plaintexts))):
+        aes = AES(keys[ix])
+        aes.encrypt(plaintexts[ix])
+
+        states[ix] = aes.state_after_round
+
+    return states
+
+
+def hamming_weights(state_list: np.ndarray):
+    """
+    Constructs a hamming weight lookup table, which it uses to transform an array of state bytes
+        into their hamming weights.
+    """
+    hw_lookup = np.zeros(BYTE_LEN, dtype=np.uint8)
+    for i in range(BYTE_LEN):
+        hw_lookup[i] = hw(i)
+
+    return hw_lookup[state_list]

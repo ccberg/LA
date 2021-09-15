e21d554b13114adc934deec8e51a041e9c36bd70
Chris Berg <contact@ccberg.nl>
2021-09-15 12:04:21 +0200

    PoI graph update

--- src/dlla/berg.py
+++ src/dlla/berg.py
@@ -7,35 +7,36 @@ NUM_CLASSES = 9
 
 def mlp_best(x, y, params):
     """
-    Based on MLP_best from Benadjila et al. (2020)
+    Returns a trained MLP using a tunable set of parameters
     """
     mdl = Sequential()
-    mdl.add(Dense(200, activation=params['activation'], input_shape=(x.shape[1],)))
-    mdl.add(Dense(200, activation=params['activation']))
-    mdl.add(Dense(200, activation=params['activation']))
-    mdl.add(Dense(200, activation=params['activation']))
-    mdl.add(Dense(200, activation=params['activation']))
+
+    assert len(params['hidden_layers']) > 1
+
+    mdl.add(Dense(params['hidden_layers'][0], activation=params['activation'], input_shape=(x.shape[1],)))
+    for nodes in params['hidden_layers'][1:]:
+        mdl.add(Dense(nodes, activation=params['activation']))
+
     mdl.add(Dense(params['num_classes'], activation='softmax'))
 
     mdl.compile(optimizer=params['optimizer'], loss=params['losses'], metrics=['accuracy'])
 
-    mdl.fit(x, y, shuffle=True, batch_size=params['batch_size'], epochs=params['epochs'],
-                  verbose=params['progress'])
+    mdl.fit(x, y, shuffle=True, batch_size=params['batch_size'], epochs=params['epochs'], verbose=params['progress'])
 
     return mdl
 
 
 def make_mlp(x, y, num_classes=NUM_CLASSES, progress=True):
     """
-    Returns a trained MLP using a tunable set of parameters
+    Based on MLP_best from Benadjila et al. (2020)
     """
     return mlp_best(x, y, {
         'activation': 'relu',
         'optimizer': Adam(learning_rate=0.001),
         'losses': 'categorical_crossentropy',
+        'hidden_layers': [200] * 5,
         'batch_size': 200,
         'epochs': 5,
         'progress': progress,
         'num_classes': num_classes
     })
-
